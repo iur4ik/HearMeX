1e8aafcc724b07af0e19014c8d5ebb651da6c7c7
iur4ik <iboicenco@pppoe-212.28.75.94.araxinfo.com>
2014-08-18 23:31:04 +0300

    Created the initial code in order to create the sound recording and playback features.

--- HearMeNow/ViewController.m
+++ HearMeNow/ViewController.m
@@ -9,6 +9,13 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    BOOL hasRecording;
+    AVAudioPlayer *soundPlayer;
+    AVAudioRecorder *soundRecorder;
+    AVAudioSession *session;
+    NSString *soundPath;
+}
 
 @end
 
@@ -17,13 +24,93 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    soundPath = [[NSString alloc] initWithFormat:@"%@%@", NSTemporaryDirectory(), @"hearmenow.wav"];
+    NSURL *url = [[NSURL alloc] initFileURLWithPath:soundPath];
+    session = [AVAudioSession sharedInstance];
+    [session setActive:YES error:nil];
+    NSError *error;
+    
+    [[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayAndRecord
+                                           error:&error];
+    
+    soundRecorder = [[AVAudioRecorder alloc] initWithURL:url
+                                                settings:nil
+                                                   error:&error];
+    if (error) {
+        NSLog(@"Error while initializing the recorder: %@", error);
+    }
+    soundRecorder.delegate = self;
+    [soundRecorder prepareToRecord];
+	
 }
 
-- (void)didReceiveMemoryWarning
+- (IBAction)recordPressed:(id)sender
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    if ([soundRecorder isRecording])
+    {
+        [soundRecorder stop];
+        [self.recordButton setTitle:@"Record"
+                           forState:UIControlStateNormal];
+    }
+    else
+    {
+        [session requestRecordPermission:^(BOOL granted)
+        {
+            if(granted)
+            {
+                [soundRecorder record];
+                [self.recordButton setTitle:@"Stop"
+                                   forState:UIControlStateNormal];
+            }
+            else
+            {
+                NSLog(@"Unable to record");
+            }
+        }];
+    }
 }
+
+- (IBAction)playPressed:(id)sender
+{
+    if (soundPlayer.playing)
+    {
+        [soundPlayer pause];
+        [self.playButton setTitle:@"Play" forState:UIControlStateNormal];
+    }
+    else if (hasRecording)
+    {
+        NSURL *url = [[NSURL alloc] initFileURLWithPath:soundPath];
+        NSError *error;
+        soundPlayer = [[AVAudioPlayer alloc] initWithContentsOfURL:url error:&error];
+        if (!error)
+        {
+            soundPlayer.delegate = self;
+            [soundPlayer play];
+        }
+        else
+        {
+            NSLog(@"Error initializing player: %@", error);
+        }
+        [self.playButton setTitle:@"Pause" forState:UIControlStateNormal];
+        hasRecording = NO;
+    }
+    else if (soundPlayer)
+    {
+        [soundPlayer play];
+        [self.playButton setTitle:@"Pause" forState:UIControlStateNormal];
+    }
+}
+
+- (void)audioRecorderDidFinishRecording:(AVAudioRecorder *)recorder successfully:(BOOL)flag;
+{
+    hasRecording = flag;
+    [self.recordButton setTitle:@"Record" forState:UIControlStateNormal];
+}
+
+- (void)audioPlayerDidFinishPlaying:(AVAudioPlayer *)player successfully:(BOOL)flag;
+{
+    [self.playButton setTitle:@"Play" forState:UIControlStateNormal];
+}
+
 
 @end
