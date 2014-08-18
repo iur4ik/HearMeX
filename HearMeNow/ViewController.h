7b174ff58c06d2586dc199f645ad890fb984686d
iur4ik <iboicenco@pppoe-212.28.75.94.araxinfo.com>
2014-08-18 20:46:50 +0300

    Initial Commit

---
+++ HearMeNow/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HearMeNow
+//
+//  Created by iboicenco on 8/18/14.
+//  Copyright (c) 2014 iboicenco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ HearMeNow/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HearMeNow
+//
+//  Created by iboicenco on 8/18/14.
+//  Copyright (c) 2014 iboicenco. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
